a249428d176bf80cfb2ef9ebbcbf896ca95a38e9
avi-08 <avi.1199863@gmail.com>
2018-08-02 15:00:05 +0530

    Added comment

--- app.py
+++ app.py
@@ -2,5 +2,8 @@ from web import create_app
 
 
 if __name__ == '__main__':
+    """
+    the main function gets executed when app is run
+    """
     app = create_app(debug=False)
     app.run(host='0.0.0.0', port=5000)
